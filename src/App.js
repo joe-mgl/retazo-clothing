7410c9363c29e7544771f6383883097950a7d9f6
joe-mgl <otepmigue@gmail.com>
2020-11-19 11:26:16 -0800

    added header component with links to shop and contact pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+    	<Header />
+    	<Switch>
+		    <Route exact path='/' component={HomePage} />
+		    <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
